b5024cf2ebc112ba319ebdc6d8691ac3a7e8284b
leo_rsnd <rsnd.leo@gmail.com>
2023-09-04 14:23:08 -0300

    .env variables fix

--- db/db/settings.py
+++ db/db/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('STUDIOUS_SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
